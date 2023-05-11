9e4abe7946581d0b673ad2b1ffd1736718f176dd
enginksaa <enginksaa@gmail.com>
2023-05-11 23:32:23 +0300

    WeatherEventAdvice
    Signed-off-by: enginksaa <enginksaa@gmail.com>

---
+++ src/AdviceEvent.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class AdviceEvent {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lütfen sıcaklık değerini girin: ");
+        int heat = scanner.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Hava çok soğuk, yani kayak için mükemmel bir gün.");
+        } else if (heat >= 5 && heat <= 15) {
+            System.out.println("Hava serin, söyle patlamış mısırlı bir filme ne dersin ?.");
+        } else if (heat> 15 && heat <= 25) {
+            System.out.println("Hava güzel, arkadaslarla piknige ne dersin?.");
+        } else {
+            System.out.println("Hava sıcak, herkes havuza serinleme zamanı.");
+        }
+
+
+
+
+
+
+    }
+}
