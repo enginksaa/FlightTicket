8967f16b5c0f3adce7d6f161d32ecc51dbfb3ac3
enginksaa <enginksaa@gmail.com>
2023-05-12 00:22:35 +0300

    ArtikYilHesaplama
    Signed-off-by: enginksaa <enginksaa@gmail.com>

---
+++ src/ArtikYil.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ArtikYil {
+
+    public static void main(String[] args) {
+
+        Scanner userEntry = new Scanner(System.in);
+        int year;
+        char devam;
+
+        do {
+            System.out.print("Yıl Giriniz : ");
+            year = userEntry.nextInt();
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println(year + " bir artık yıldır !");
+            } else {
+                System.out.println(year + " bir artık yıl değildir !");
+            }
+
+            System.out.print("Devam etmek istiyor musunuz? (E/H): ");
+            devam = userEntry.next().charAt(0);
+
+        } while (devam == 'E' || devam == 'e');
+
+        userEntry.close();
+    }
+}
+
